af4df36ca0d913fdb5bfd50394a239b45fd72e9e
arwa12-bm <arwaarij12344@gmail.com>
2024-02-20 15:14:35 +0100

    feat: add/use  `CartProvider` in `layout` component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,14 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
+
+import { UserLocationProvider } from "./hooks/useLocation";
+import { CardProvider } from "./hooks/useCard";
 import NavBar from "./components/nav/NavBar";
 import Footer from "./components/footer/Footer";
-import { UserLocationProvider } from "./hooks/useLocation";
+
 
 import "./globals.css";
-
-
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -25,11 +26,13 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${inter.className} text-slate-700`}>
       <UserLocationProvider>
+        <CardProvider>
         <div className="flex flex-col min-h-screen">
           <NavBar isClick={false}/>
           <main className="flex-grow">{children}</main>
           <Footer/>
       </div>
+      </CardProvider>
       </UserLocationProvider>
         </body>
     </html>
