649b496db654a1ebc1ad5a4c6a335e811e886526
arwa12-bm <arwaarij12344@gmail.com>
2024-02-20 15:06:28 +0100

    refactor: review `Product getIdShopList` function in `product page ` component

--- app/product/[productId]/page.tsx
+++ app/product/[productId]/page.tsx
@@ -1,46 +1,38 @@
 "use client"
 
+import { useEffect } from "react";
+
 import Container from "@/app/components/Container";
 import HomePhoto from "@/app/components/HomePhoto";
 import CategorieCart from "@/app/components/product/CategorieCart";
 import MenuCategorie from "@/app/components/product/MenuCategorie";
-import ProductCart from "@/app/components/product/ProductCart";
-import { DropdownApp } from "@/app/components/product/dropDown";
+import useCard from "@/app/hooks/useCard";
 import { card } from "@/app/utils/products";
-import { useEffect, useState } from "react";
+
 
 interface Iparams{
     productId?:string
 }
 
 const Product= ({params}:{params:Iparams}) => {
-    console.log("params",params.productId);
-    let selectedProduct:any
-    for (let item in Object.keys(card.shoplist)) {
-        console.log("cc",(Object.keys(card.shoplist)as any)[item]),"cc1",JSON.stringify(params.productId);
-        
-        if (JSON.stringify((Object.keys(card.shoplist)as any)[item]) === JSON.stringify(params.productId) ) {
-            selectedProduct  =  Object.values(card.shoplist as any)[item]
-           // console.log("xxx",Object.values(card.shoplist as any)[item])
-        }
-    }
-    console.log("selectedProduct",selectedProduct)
+
+    const {selectedIdShopList,getSelectedIdShopList,getIdShopList,Id} = useCard()
+    
+    useEffect(()=>{
+        getSelectedIdShopList(params)
+    },[])
+    
+    
     return ( 
     <div className="flex flex-col">
-    <HomePhoto  data={selectedProduct}  />
-    <MenuCategorie  />
+    <HomePhoto  data={selectedIdShopList}  />
+    <MenuCategorie data={selectedIdShopList}  />
     <Container>
-        
-        <div className="grid grid-cols-2  gap-8 m-8">
-
-            {Object.values(card.categories).map((item)=><div  className=""><CategorieCart  data={item} /></div>)}
-        
+        <div className="grid grid-cols-1   md:grid-cols-2 sm:grid-cols-2 gap-8 m-8">
+            {Object.values(card.categories).map((item)=><div key={item.id}  className=""> <CategorieCart  data={item} Id={Id} /></div>)}
         </div>
     </Container>
     </div>
-
-        
-        
         );
 }
 
