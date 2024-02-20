8edf8862b957038601c437a5b37aaca023fd17b2
arwa12-bm <arwaarij12344@gmail.com>
2024-02-20 15:29:38 +0100

    feat: add/use `getIdShopList` function in `ProductCart` component

--- app/components/product/ProductCart.tsx
+++ app/components/product/ProductCart.tsx
@@ -1,28 +1,24 @@
 "use client";
+
+import { useEffect } from "react";
 import Image from "next/image";
 import { MdOutlinePayments } from "react-icons/md";
 import { LuBadgeDollarSign } from "react-icons/lu";
 import { useRouter } from "next/navigation";
-import { card } from "@/app/utils/products";
+
+import useCard from "@/app/hooks/useCard";
 
 interface ProductCartProps {
   data: any;
 }
 const ProductCart: React.FC<ProductCartProps> = ({ data }) => {
   const router = useRouter();
-  //let id:any
-  //   const id = Object.values(card.shoplist).find(
-  //     (item: any) =>
-  //       JSON.stringify(item) === data)
-  let id = "";
-  for (let item of Object.keys(card.shoplist)) {
-    if (JSON.stringify((card.shoplist as any)[item]) === JSON.stringify(data)) {
-      id = item;
-    }
-  }
+  const {Id,getIdShopList} = useCard()
+  useEffect(() => {
+    getIdShopList(data)
+  }, []);
+  
 
-  // console.log("data xx",data)
-  console.log("id xx", id);
   return (
     <div
       className="relative col-span-1 h-full cursor-pointer border-[1.2px] border-slate-200 bg-slate-50 rounded-sm
@@ -34,7 +30,7 @@ const ProductCart: React.FC<ProductCartProps> = ({ data }) => {
             fill
             src={data.image}
             alt=""
-            onClick={() => router.push(`/product/${id}`)}
+            onClick={() => router.push(`/product/${Id}`)}
           />
         </div>
         <div className="grid grid-cols-2  justify-between  text-[15px] font-semibold px-1  w-full  text-green-600">
