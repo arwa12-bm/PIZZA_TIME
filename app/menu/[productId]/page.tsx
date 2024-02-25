5b93f417e6e32220244872d57e8025e8897acdef
arwa12-bm <arwaarij12344@gmail.com>
2024-02-25 19:24:35 +0100

    refactor:review logic component in `menu/page` component

--- app/menu/[productId]/page.tsx
+++ app/menu/[productId]/page.tsx
@@ -1,12 +1,12 @@
 "use client"
 
-import MenuCategorie from "../../components/product/MenuCategorie";
-import { card } from "@/app/utils/products";
-import ProductCategorie from "@/app/components/product/ProductCategorie";
 import { useEffect, useState } from "react";
+
+import MenuCategorie from "../../components/categorie/MenuCategorie";
+import ProductCategorie from "@/app/components/categorie/ProductCategorie";
 import useCard from "@/app/hooks/useCard";
 import HomePhoto from "@/app/components/HomePhoto";
-import { log } from "console";
+
 
 interface Iparams{
     productId?:string
@@ -15,9 +15,6 @@ interface Iparams{
 const Menu = ({params}:{params?:Iparams}) => {
     
     const {selectedIdShopList,getSelectedIdCategorieList,selectedElCategorie} = useCard()
-    console.log(params?.productId)
-
-   
 
     useEffect(()=>{
         getSelectedIdCategorieList(params)
@@ -25,13 +22,15 @@ const Menu = ({params}:{params?:Iparams}) => {
     
 
     return ( 
+    
     <div className="flex flex-col">
-    <HomePhoto  data={selectedIdShopList}  />
-    <MenuCategorie  />
-    <div className="grid grid-cols-1   md:grid-cols-2 sm:grid-cols-2 gap-8 m-8">
-    {(selectedElCategorie?.items)?.map((item:any)=> <ProductCategorie  data={item} />)}
-    </div>
-    </div> );
+        <HomePhoto  data={selectedIdShopList}  />
+        <MenuCategorie  />
+        <div className="grid grid-cols-1   md:grid-cols-2 sm:grid-cols-2 gap-8 m-8">
+            {(selectedElCategorie?.items)?.map((item:any)=> <ProductCategorie  data={item} />)}
+        </div>
+    </div> 
+    );
 }
 
 export default Menu;
