48345bd1dcc0a58dd0479faba921c603dca0b989
arwa12-bm <arwaarij12344@gmail.com>
2024-02-27 11:31:29 +0100

    feat: add/use `InputSupp`  component in `InputSupp` called in `ProductModal`

---
+++ app/components/form/InputSupp.tsx
@@ -0,0 +1,34 @@
+"use client"
+import { Checkbox, FormControlLabel } from "@mui/material";
+import NumberControl from "../categorie/InputNumber";
+import { useState } from "react";
+import { formatPrice } from "@/app/utils/formatPrice";
+
+
+interface InputSuppProps{
+    item:any
+    index?:number
+
+    onchangeList:(item:any,SuppItems:any,newCount:any)=>any
+}
+const InputSupp:React.FC<InputSuppProps> = ( {item,index,onchangeList}) => {
+    const [count, setCount] =useState(0);
+    const [SuppItems, setSuppItems] = useState({});    
+
+    const onCountChanged = (newCount: number) => {
+        setCount(newCount);
+        let sup:any=localStorage.getItem("supList")!==null?JSON.parse(localStorage.getItem("supList")??'{}'):{}
+        setSuppItems(onchangeList(item,sup,newCount))
+            
+    };
+
+    return ( 
+        <div key={index} className='flex justify-content justify-between border-[1px] rounded-lg '>
+                                        <FormControlLabel control={<Checkbox checked={count > 0} />} value={count} label={item.title}  />
+                                        <NumberControl value={count} onChange={onCountChanged} />
+                                        <p className='p-2'>{formatPrice(item.price * count)}</p>
+                                    </div>
+    );
+}
+
+export default InputSupp;
