e8de1d2d7d7e074ce9ab5fa1ed7b6384c2b352e0
arwa12-bm <arwaarij12344@gmail.com>
2024-02-27 11:27:38 +0100

    feat: add/use `ModeRetrait` Props from  localStorage and `handleTimeChange` function in `TimePickerApp` component

--- app/components/product/TimePicker.tsx
+++ app/components/product/TimePicker.tsx
@@ -1,9 +1,18 @@
 
 import dayjs from 'dayjs';
 import { TimePicker } from 'antd';
+import { useState } from 'react';
 
 const format = 'HH:mm';
+interface handleTimeChangeProps{
+    handleTimeChange?:(time:any)=>void
+    ModeRetrait?:any
+}
+const TimePickerApp: React.FC<handleTimeChangeProps> = ({ ModeRetrait ,handleTimeChange}) => {
 
-const TimePickerApp: React.FC = () => <TimePicker defaultValue={dayjs('12:08', format)} format={format} minuteStep={15}  />;
+return(
+<TimePicker defaultValue={Object.keys(ModeRetrait).length === 0 ? dayjs("12:00", format): dayjs( ModeRetrait.Time, format) } onChange={handleTimeChange} format={format} minuteStep={15}  className='w-[100px] h-full'/>
+)}
+
 
 export default TimePickerApp;
