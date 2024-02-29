4e0c93b51435b51e996b26b5166f80abefb67962
arwa12-bm <arwaarij12344@gmail.com>
2024-02-29 12:39:04 +0100

    refactor:review   `handleValider`  function in `ProductModal` component

--- app/components/categorie/ProductModal.tsx
+++ app/components/categorie/ProductModal.tsx
@@ -22,54 +22,50 @@ interface ProductModalAppProps{
 const ProductModalApp: React.FC<ProductModalAppProps> = ({Open,onClose,img,data,CompList}) => {
     if (!Open) return null
 
-    const [big,setBig]=useState(true)
     const [loading, setLoading] = useState(false);
     const [checkedItems, setCheckedItems] = useState({CREMEFRAICHE:true,FROMAGE:true,OIGNON:true});
+
     const{handleAddProductToCart,HandleCartQtyIncrease,cartProducts}=useCard()
 
+//created suppList in localStorage
     const handleSuppChange=(item:any,SuppItems:any,newCount:any)=>
         {const newCheckedItems:any = {...SuppItems};
-        if (newCheckedItems[item.title] === undefined || newCheckedItems[item.title] >= 0  ) {
+        if (newCheckedItems[item.title] === undefined || newCheckedItems[item.title] > 0  ) {
         newCheckedItems[item.title] = newCount;
         }
         localStorage.setItem("supList",JSON.stringify(newCheckedItems))}
         
-
+    
+//on validate modal create ItemList{sup,checkedItems,data}  
     const handleValider=()=>{
-        let sup:any=localStorage.getItem("supList")!==null?JSON.parse(localStorage.getItem("supList")??'{}'):{}
+        let sup1:any=localStorage.getItem("supList")!==null?JSON.parse(localStorage.getItem("supList")??'{}'):{}
+        let sup: any = {};
+        // ignore the item has 0 value
+        for (let item of Object.keys(sup1)) {
+            if (sup1.hasOwnProperty(item) && sup1[item] !== 0) {
+                sup[item] = sup1[item];
+            }
+        }
+
         localStorage.setItem("ItemList",JSON.stringify({sup,checkedItems,data}))
-        const Existingindex =cartProducts?.findIndex((item:any)=>  JSON.stringify(item.data) === JSON.stringify(data))
+        const Existingindex =cartProducts?.findIndex((item:any)=>  JSON.stringify(item.data) === JSON.stringify(data) //same item
+                                                                    && JSON.stringify(item.sup) === JSON.stringify(sup)  //same suppliment List
+                                                                    && JSON.stringify(item.checkedItems) === JSON.stringify(checkedItems) ) //same (composant de base )
+        
+        //add to cart new item 
         if( Existingindex == -1 || cartProducts == null){
             handleAddProductToCart({sup,checkedItems,data,quantity:1})
             localStorage.setItem("supList",JSON.stringify(null))
             localStorage.setItem("ItemList",JSON.stringify(null))
         }else{
+        //increase quantity
             HandleCartQtyIncrease(cartProducts[Existingindex])
 
         }
         setLoading(true)
         onClose()
         }
-    
 
-    
-        
-    useEffect(() => {
-        
-        const handleResize = () => {
-            if (window.innerWidth < 760) {
-                setBig(false)
-                
-            } else {
-                setBig(true)
-            }
-        };
-
-        window.addEventListener("resize", handleResize);
-        return () => {
-            window.removeEventListener("resize", handleResize);
-        };
-    }, []);
 
     return (
         <>
@@ -104,13 +100,16 @@ const ProductModalApp: React.FC<ProductModalAppProps> = ({Open,onClose,img,data,
                         <p>COMPOSITION DE BASE</p>
                         <FormGroup className='flex gap-2'>
                             {Object.values(CompList).map((item:any)=>
-                                <FormControlLabel key={item.title} control={<Checkbox value={item.title} 
+                            
+                                <FormControlLabel key={item.title} control={
+                                                                        <Checkbox value={item.title} 
                                                                         onChange={e => {
+                                                                            const modifiedTitle = item.title ? item.title.replace(/\s/g, '') : '';
                                                                             const newCheckedItems:any = {...checkedItems};
-                                                                            if (newCheckedItems[item.title] === undefined || e.target.checked) {
-                                                                            newCheckedItems[item.title] = true;
+                                                                            if (newCheckedItems[modifiedTitle] === undefined || e.target.checked) {
+                                                                            newCheckedItems[modifiedTitle] = true;
                                                                             } else {
-                                                                            delete newCheckedItems[item.title];
+                                                                            delete newCheckedItems[modifiedTitle];
                                                                             }
                                                                             setCheckedItems(newCheckedItems);
                                                                         }}
@@ -124,10 +123,13 @@ const ProductModalApp: React.FC<ProductModalAppProps> = ({Open,onClose,img,data,
                         <p className='text-sm'>Choisissez jusqu'à 9</p>
                         <FormGroup className='flex gap-2'>
                             {Object.values(card.SupplimentComposition)?.map((item:any,index:number)=>
-                                    <InputSupp item={item} 
+                                    <div key={index}>
+                                        <InputSupp item={item} 
                                                 index={index}
                                                 onchangeList={handleSuppChange}
                                                 />
+                                    </div>
+                                    
                                 
                                 )}
                         </FormGroup>
