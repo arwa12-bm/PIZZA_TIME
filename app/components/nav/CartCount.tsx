b0311108d516177cf2f664bccbbf2bc6b8623037
arwa12-bm <arwaarij12344@gmail.com>
2024-02-29 09:54:41 +0100

    feat: add `CartCount` function in `nav/cartCount` component

---
+++ app/components/nav/CartCount.tsx
@@ -0,0 +1,20 @@
+'use client'
+import useCard from "@/app/hooks/useCard";
+import { useRouter } from "next/navigation";
+import { CiShoppingCart } from "react-icons/ci";
+
+const CartCount = () => {
+    const  {cartTotalQty} =useCard()
+    const router =useRouter()
+    return ( 
+    <div className="absolute cursor-pointer" onClick={()=>router.push('/cart')}>
+        <div className="text-3xl">
+            <CiShoppingCart />
+        </div>
+        <span className="absolute top-[-10px] right-[-10px] bg-slate-700 h-6 w-6 rounded-full flex items-center justify-center text-sm text-white">
+        {cartTotalQty}
+        </span>
+    </div> );
+}
+
+export default CartCount;
