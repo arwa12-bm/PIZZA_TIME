89e13b52b435aabe1bcfad81fe49e7c718f5f9d9
arwa12-bm <arwaarij12344@gmail.com>
2024-02-29 09:53:40 +0100

    feat: use `CartCount` function  in `NavBar` component

--- app/components/nav/NavBar.tsx
+++ app/components/nav/NavBar.tsx
@@ -5,6 +5,7 @@ import Link from "next/link";
 import { MdMenu } from "react-icons/md";
 import { useCallback, useState } from "react";
 import FormCnx from "./FormCnx";
+import CartCount from "./CartCount";
 
 interface NavBarProps{
     isClick?:boolean
@@ -29,16 +30,18 @@ const NavBar : React.FC<NavBarProps> = ({isClick}) => {
 
                 <Container>
                 
-                    <div className='flex    w-full item-center justify-between gap-10  right-0 left-0'>
+                    <div className='grid grid-cols-3  w-full items-center justify-between gap-4  right-0 left-0'>
                         
                             <div >
                                 <Link href="/"  >           
                                 <Image  src="/logo.png"  alt="logo"  width={"120"} height={"120"} />
                                 </Link>
                             </div>
-                            
+                            <div className=" w-full mb-4  px-10">
+                            <CartCount />
+                            </div>
                             <button  onClick={handleMenu}>
-                                <MdMenu size={35} className="border-[2px] mt-3  border-slate-800 cursor-pointer rounded-md p-0.8"/>
+                                <MdMenu size={35} className="border-[2px] mt-2  border-slate-800 cursor-pointer rounded-md p-0.8"/>
                             </button>
                     </div>
             </Container>
