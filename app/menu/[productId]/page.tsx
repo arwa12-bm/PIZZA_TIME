c8ae99915f1aab4864faa7e4cc33ec5e4d8334dd
arwa12-bm <arwaarij12344@gmail.com>
2024-03-08 09:50:56 +0100

    refactor:review  logic component in `Menu/page` component

--- app/menu/[productId]/page.tsx
+++ app/menu/[productId]/page.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import { useEffect } from "react";
+import { useParams } from "next/navigation";
 
 import MenuCategorie from "../../components/categorie/MenuCategorie";
 import ProductCategorie from "@/app/components/categorie/ProductCategorie";
@@ -8,12 +9,8 @@ import useCard from "@/app/hooks/useCard";
 import HomePhoto from "@/app/components/HomePhoto";
 
 
-interface Iparams{
-    productId?:string
-}
-
-const Menu = ({params}:{params?:Iparams}) => {
-    
+const Menu = () => {
+    const params = useParams();
     const {selectedIdShopList,getSelectedIdCategorieList,selectedElCategorie} = useCard()
 
     useEffect(()=>{
@@ -25,7 +22,7 @@ const Menu = ({params}:{params?:Iparams}) => {
     
     <div className="flex flex-col">
         <HomePhoto  data={selectedIdShopList}  />
-        <MenuCategorie  />
+        <MenuCategorie />
         <div className="grid grid-cols-1   md:grid-cols-2 sm:grid-cols-2 gap-8 m-8">
             {(selectedElCategorie?.items)?.map((item:any)=> <div key={item}><ProductCategorie  data={item} /></div>)}
         </div>
