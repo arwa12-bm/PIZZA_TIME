e43a14babe2d86d225cc0b9c18d0d9d21d6ab13b
arwa12-bm <arwaarij12344@gmail.com>
2024-03-08 09:48:42 +0100

    refactor:review  logic component in `Product/page` component

--- app/product/[productId]/page.tsx
+++ app/product/[productId]/page.tsx
@@ -1,47 +1,60 @@
-"use client"
+    "use client";
 
-import { useEffect,useState } from "react";
-
-import Container from "@/app/components/Container";
-import HomePhoto from "@/app/components/HomePhoto";
-import CategorieCart from "@/app/components/categorie/CategorieCart";
-import MenuCategorie from "@/app/components/categorie/MenuCategorie";
-import useCard from "@/app/hooks/useCard";
-import { card } from "@/app/utils/products";
+    import { useEffect, useState } from "react";
+    import { useParams } from "next/navigation";
 
 
-interface Iparams{
-    productId?:string
-}
-
-const Product= ({params}:{params:Iparams}) => {
-    const[selectedProductId ,setSelectedProductId ]=useState({})
-    const[selectedProductData ,setSelectedProductData ]=useState({})
-
-    const {getSelectedIdShopList} = useCard()
-    
-    useEffect(()=>{ 
-        getSelectedIdShopList(params)
-        setSelectedProductId(localStorage.getItem("selectedProductId")!==null?JSON.parse(localStorage.getItem("selectedProductId")??'{}'):{})
-        setSelectedProductData(localStorage.getItem("selectedProductData")!==null?JSON.parse(localStorage.getItem("selectedProductData")??'{}'):{})
-    },[])
-
-    
+    import Container from "@/app/components/Container";
+    import HomePhoto from "@/app/components/HomePhoto";
+    import CategorieCart from "@/app/components/categorie/CategorieCart";
+    import MenuCategorie from "@/app/components/categorie/MenuCategorie";
+    import useCard from "@/app/hooks/useCard";
+    import { card } from "@/app/utils/products";
 
 
+    const Product = () => {
+    const params = useParams();
+    const [selectedProductId, setSelectedProductId] = useState({});
+    const [selectedProductData, setSelectedProductData] = useState({});
+    const [data, setData] = useState(null);
 
-    return ( 
-    <div className="flex flex-col">
-    <HomePhoto  data={selectedProductData }  />
-    <MenuCategorie data={selectedProductData }  />
-    <Container>
-        <div className="grid grid-cols-1   md:grid-cols-2 sm:grid-cols-2 gap-8 m-8">
-            {Object.values(card.categories).map((item:any)=><div key={item.id}  className="" > <CategorieCart  data={item} Id={selectedProductId}  /></div>)}
-            
-        </div>
-    </Container>
-    </div>
+
+    const { getSelectedIdShopList,getData } = useCard();
+
+    useEffect(() => {
+        getSelectedIdShopList(params);
+        setSelectedProductId(
+        localStorage.getItem("selectedProductId") !== null
+            ? JSON.parse(localStorage.getItem("selectedProductId") ?? "{}")
+            : {}
         );
-}
+        setSelectedProductData(
+        localStorage.getItem("selectedProductData") !== null
+            ? JSON.parse(localStorage.getItem("selectedProductData") ?? "{}")
+            : {}
+        );
+        
+    }, []);
 
-export default Product;
+    console.log("dataBd",data);
+    
+
+    return (
+        <div className="flex flex-col">
+        <HomePhoto data={selectedProductData} />
+        <MenuCategorie  />
+        <Container>
+            <div className="grid grid-cols-1   md:grid-cols-2 sm:grid-cols-2 gap-8 m-8">
+            {Object.values(card.categories).map((item: any) => (
+                <div key={item.id} className="">
+                {" "}
+                <CategorieCart data={item} Id={selectedProductId} />
+                </div>
+            ))}
+            </div>
+        </Container>
+        </div>
+    );
+    };
+
+    export default Product;
