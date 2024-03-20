821ebb9ebe81eb63fdd3b498d0ae346d8281a9bd
arwa12-bm <arwaarij12344@gmail.com>
2024-03-20 13:56:14 +0100

    refactor:review  logic component in `CategorieCart` component

--- app/components/categorie/CategorieCart.tsx
+++ app/components/categorie/CategorieCart.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import { useState } from "react";
 import { useRouter } from "next/navigation";
 import ModeRetraitModal from "./ModalModeRetrait";
+import useCard from "@/app/hooks/useCard";
 
 
     interface CategorieCartProps{
@@ -13,10 +14,25 @@ import ModeRetraitModal from "./ModalModeRetrait";
         
     }
     const CategorieCart:React.FC <CategorieCartProps> = ({data,isTitle,Id}) => {
-        const[isClicked,setIsClicked]=useState(false)
+        const {ModeRetrait}=useCard()
+        const [isOpen,setIsOpen]=useState(false)
+        const handleOpenModal =()=>{
+            setIsOpen(true)
+        }
+        const handleModalClose = () => {
+            setIsOpen(false);
+        };
     
         const router = useRouter()
 
+        const handle=()=>{
+            
+            if (ModeRetrait === null || Object.keys(ModeRetrait).length === 0 ){
+                handleOpenModal()
+            }else{
+                router.push(`/menu/${data.id}`)} 
+
+        }
     
         return ( 
         <div>
@@ -28,7 +44,7 @@ import ModeRetraitModal from "./ModalModeRetrait";
 
             <div className=" ">
                 <div  className="flex flex-col gap-2 justify-center items-center  text-[15px] cursor-pointer hover:scale-105">
-                    <div onClick={()=>{window.location.href = `/menu/${data.id}`}} >
+                    <div onClick={handle } >
                         <div className="col-span-1 text-center font-semibold">{data.title} </div>
                         <div className="aspect-square overflow-hidden relative  rounded-md h-[280px] w-[300px]">
                             <Image
@@ -40,7 +56,7 @@ import ModeRetraitModal from "./ModalModeRetrait";
                         </div>
                     </div>
                     
-                    <ModeRetraitModal data={data} />
+                    <ModeRetraitModal Open={isOpen} onClose={handleModalClose} data={data} />
                 </div>
             </div>
             
