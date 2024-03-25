bb20ca2e44b3f32c79dd4203c0061a3e8398a845
arwa12-bm <arwaarij12344@gmail.com>
2024-03-25 14:42:11 +0100

    refactor:review  logic component in `App/page` component

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import { useEffect, useState } from "react";
 
@@ -9,41 +9,25 @@ import ProductCart from "./components/product/ProductCart";
 import SearchInput from "./components/form/SearchInput";
 import useCard from "./hooks/useCard";
 
-
 export default function Home() {
-
-  const {dataUser,getData,getCommandes,dataPanier}=useCard()
-
-
-  useEffect(()=>{
-      getData(); 
-      
-  },[])
-
-  useEffect(()=>{
-      if(dataUser!==null)
-  { getCommandes(dataUser?.id)
-      console.log("ddd",dataUser?.id);}
-      
-
-  },[dataUser])
 
   return (
     <div className="">
-    <HomePhoto />
-    <Container>
-      <div className="grid grid-cols-1 p-4 item-center   gap-4 lg:gap-12 pt-2 item-center justify-between">
-        <h1 className="text-[20px] ">Nos magasins à proximité</h1>
+      <HomePhoto />
+      <Container>
+        <div className="grid grid-cols-1 p-4 item-center   gap-4 lg:gap-12 pt-2 item-center justify-between">
+          <h1 className="text-[20px] ">Nos magasins à proximité</h1>
           <SearchInput />
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
-          {Object.values(card.shoplist).map((item:any)=><div  key={item.Company}> <ProductCart  data={item} /></div> )}
-        
-          
+            {Object.values(card.shoplist).map((item: any) => (
+              <div key={item.Company}>
+                {" "}
+                <ProductCart data={item} />
+              </div>
+            ))}
+          </div>
         </div>
-      </div>
-      
-    </Container>
+      </Container>
     </div>
-    
   );
 }
