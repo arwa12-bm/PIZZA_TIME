04b7ec79bdc22718c91d151abd4c126a363b02df
arwa12-bm <arwaarij12344@gmail.com>
2024-04-11 16:00:29 +0100

    refactor review logic component `menu` page

--- app/menu/[productId]/page.tsx
+++ app/menu/[productId]/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import { useEffect, useState } from "react";
 import { useParams } from "next/navigation";
@@ -7,41 +7,38 @@ import MenuCategorie from "../../components/categorie/MenuCategorie";
 import ProductCategorie from "@/app/components/categorie/ProductCategorie";
 import useCard from "@/app/hooks/useCard";
 import HomePhoto from "@/app/components/HomePhoto";
-import { Card } from "antd";
-
 
 const Menu = () => {
-    const params = useParams();
+const params = useParams();
 
-    const [data, setData] = useState<any>();
+const [data, setData] = useState<any>();
+const { card, selectedShoplist } = useCard();
 
-
-    const {card ,selectedShoplist,selectedCategorie,getDataCard} = useCard();
-    let selectedCat:any;
-    useEffect(()=>{
-        if(card){
-            selectedCat = card.categories?.filter((item:any)=> item.id === params.productId);
-            //console.log({selectedProduct})
-            setData(selectedCat);
-            localStorage.setItem(
-                "selectedCategorie",
-                JSON.stringify(selectedCat)
-            );
-        }
-    },[card])
-
-    //console.log({data})
-
-    return ( 
-    
-    <div className="flex flex-col">
-        {selectedShoplist && <HomePhoto  data={selectedShoplist}  />}
-        <MenuCategorie  />
-        <div className="grid grid-cols-1   md:grid-cols-2 sm:grid-cols-2 gap-8 m-8">
-            {data && data[0].items?.map((item:any)=> <div key={item}><ProductCategorie  data={item} /></div>)}
-        </div>
-    </div> 
+let selectedCat: any;
+useEffect(() => {
+if (card) {
+    selectedCat = card.categories?.filter(
+    (item: any) => item.id === params.productId
     );
+    setData(selectedCat);
+    localStorage.setItem("selectedCategorie", JSON.stringify(selectedCat));
 }
+}, [card]);
+
+return (
+<div className="flex flex-col">
+    {selectedShoplist && <HomePhoto data={selectedShoplist} />}
+    <MenuCategorie />
+    <div className="grid grid-cols-1   md:grid-cols-2 sm:grid-cols-2 gap-8 m-8">
+    {data &&
+        data[0].items?.map((item: any) => (
+        <div key={item}>
+            <ProductCategorie data={item} />
+        </div>
+        ))}
+    </div>
+</div>
+);
+};
 
 export default Menu;
